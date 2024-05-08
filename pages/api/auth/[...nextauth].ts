362f80a9c548c81eac6f0aa3376cd6503241be77
Sujan <sujanrokad44@gmail.com>
2024-05-08 03:35:07 -0400

    fix nextauth.ts to use client

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -28,7 +28,7 @@ export const authOptions: AuthOptions = {
                     throw new Error("Invalid credentials");
                 }
 
-                const user = await prisma?.user.findUnique({
+                const user = await client?.user.findUnique({
                     where: {
                         email: credentials.email,
                     },
